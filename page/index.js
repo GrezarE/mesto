4a934dd470dfb66b1f7d9d520b51715f4878ef88
Grechishnikov-Zarin Evgeniy <grecha-zarin@yandex.ru>
2021-08-17 18:56:51 +0300

    popup added

--- page/index.js
+++ page/index.js
@@ -1,9 +1,43 @@
-document.querySelector('.profile__edit').addEventListener('click', function () {
-  document.querySelector('.popup').classList.toggle('popup_opened');
+
+const content = document.querySelector('.content');
+const buttonProfile = document.querySelector('.profile__button');
+const buttonProfileEdit = document.querySelector('.profile__edit');
+const popupProfile = document.querySelector('.profile__popup');
+const profileForm = document.querySelector('.profile__form')
+const formElement = document.querySelector('.element__popup');
+const closeElement = document.querySelector('.element__close');
+const closeProfile = document.querySelector('.profile__close')
+const nameInput = document.querySelector('input[name="profile"]');
+const jobInput = document.querySelector('input[name="occupation"]');
+let titleProfile = document.querySelector('.profile__title');
+let jobProfile = document.querySelector('.profile__text')
+
+buttonProfile.addEventListener('click', function () {
+  formElement.classList.toggle('popup_opened');
 }, false);
 
-
-document.querySelector('.popup__close').addEventListener('click', function () {
-  document.querySelector('.popup').classList.remove('popup_opened');
+closeElement.addEventListener('click', function () {
+  formElement.classList.remove('popup_opened');
 }, false);
+
+buttonProfileEdit.addEventListener('click', function () {
+  popupProfile.classList.toggle('popup_opened');
+}, false);
+
+closeProfile.addEventListener('click', function () {
+  popupProfile.classList.remove('popup_opened');
+}, false);
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  let jobValue = jobInput.value;
+  let nameValue = nameInput.value;
+  titleProfile.textContent = nameValue;
+  jobProfile.textContent = jobValue;
+  popupProfile.classList.toggle('popup_opened');
+}
+
+profileForm.addEventListener('submit', formSubmitHandler);
+
+
 
